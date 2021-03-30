c571b5dfe8acfecccc0b3479676dd11556ac6f9b
mathiasvonende <mathias@vonende.de>
2021-03-30 22:15:22 +0200

    refactor: geometry test

--- test/Geometry.test.ts
+++ test/Geometry.test.ts
@@ -2,7 +2,7 @@
 import { Just, Nothing } from "purify-ts";
 
 import { Coordinates } from "../src/lib/Coordinates";
-import { geometryTypes, validateFeatureGeometry } from "../src/lib/Geometry";
+import { geometryTypes, validateGeometry } from "../src/lib/Geometry";
 import { record } from "../src/lib/Shared";
 
 describe("Geometry", () => {
@@ -18,95 +18,61 @@ describe("Geometry", () => {
     });
   });
 
-  describe("validateFeatureGeometry(geom)", () => {
+  describe("validateGeometry(geom)", () => {
     it("returns Just(geom) if geom is a valid geometry", () => {
       const point: Coordinates = [0.0, 1.2];
-      const pointGeom: record = {
-        type: "Point",
-        coordinates: point,
-      };
-      expect(validateFeatureGeometry(pointGeom)).toEqual(Just(pointGeom));
+      const pointGeom: record = { type: "Point", coordinates: point };
+      expect(validateGeometry(pointGeom)).toEqual(Just(pointGeom));
 
       const pointArray: Coordinates = [[0, 0], point, [11.2, 1]];
-      const lineGeom = {
-        type: "LineString",
-        coordinates: pointArray,
-      };
-      expect(validateFeatureGeometry(lineGeom)).toEqual(Just(lineGeom));
+      const lineGeom = { type: "LineString", coordinates: pointArray };
+      expect(validateGeometry(lineGeom)).toEqual(Just(lineGeom));
 
       const lineArray: Coordinates = [[[0, 0], point], pointArray, pointArray];
-      const mlineGeom = {
-        type: "MultiLineString",
-        coordinates: lineArray,
-      };
-      const polyGeom = {
-        type: "Polygon",
-        coordinates: lineArray,
-      };
-      expect(validateFeatureGeometry(mlineGeom)).toEqual(Just(mlineGeom));
-      expect(validateFeatureGeometry(polyGeom)).toEqual(Just(polyGeom));
+      const mlineGeom = { type: "MultiLineString", coordinates: lineArray };
+      const polyGeom = { type: "Polygon", coordinates: lineArray };
+      expect(validateGeometry(mlineGeom)).toEqual(Just(mlineGeom));
+      expect(validateGeometry(polyGeom)).toEqual(Just(polyGeom));
 
       const polyArray: Coordinates = [lineArray, lineArray];
-      const multipolyGeom = {
-        type: "MultiPolygon",
-        coordinates: polyArray,
-      };
-      expect(validateFeatureGeometry(multipolyGeom)).toEqual(
-        Just(multipolyGeom)
-      );
-
-      expect(validateFeatureGeometry(null)).toEqual(Just(null));
+      const multipolyGeom = { type: "MultiPolygon", coordinates: polyArray };
+      expect(validateGeometry(multipolyGeom)).toEqual(Just(multipolyGeom));
+      expect(validateGeometry(null)).toEqual(Just(null));
     });
 
-    it("returns Nothing if geom is an invalid geometry", () => {
-      expect(validateFeatureGeometry(<record>(<unknown>undefined))).toBe(
-        Nothing
-      );
+    // it("returns Nothing if geom is an invalid geometry", () => {
+    it("returns Nothing if geom is undefined", () =>
+      expect(validateGeometry(<record>(<unknown>undefined))).toBe(Nothing));
 
+    it("returns Nothing if geom has invalid coordinates", () =>
       expect(
-        validateFeatureGeometry({
-          type: null,
-          coordinates: "Thunk!",
-        })
-      ).toEqual(Nothing);
+        validateGeometry({ type: "Point", coordinates: "Thunk!" })
+      ).toEqual(Nothing));
 
+    it("returns Nothing if geom has an invalid type", () =>
       expect(
-        validateFeatureGeometry({
-          type: null,
-          coordinates: [1, 2, 3, 4, 5, 6, 7, 9],
-        })
-      ).toEqual(Nothing);
+        validateGeometry({ type: null, coordinates: [1, 2, 3, 4, 5, 6, 7, 9] })
+      ).toEqual(Nothing));
 
+    it("returns Nothing if lat coordinates exceed 180°", () =>
       expect(
-        validateFeatureGeometry({
-          type: "Point",
-          coordinates: null,
-        })
-      ).toEqual(Nothing);
+        validateGeometry({ type: "Point", coordinates: [[190.2, 88.3]] })
+      ).toEqual(Nothing));
 
+    it("returns Nothing if lon coordinates exceed -90°", () =>
       expect(
-        validateFeatureGeometry({
-          type: "Point",
-          coordinates: [[190.2, 88.3]],
-        })
-      ).toEqual(Nothing);
+        validateGeometry({ type: "Point", coordinates: [19.2, -98.3] })
+      ).toEqual(Nothing));
 
+    it("returns Nothing if a coordinate value is null", () =>
       expect(
-        validateFeatureGeometry({
-          type: "Point",
-          coordinates: [190.2, 88.3],
-        })
-      ).toEqual(Nothing);
-
-      expect(
-        validateFeatureGeometry({
+        validateGeometry({
           type: "MultiPoint",
           coordinates: [
             [0, 0],
             [0, null],
           ],
         })
-      ).toEqual(Nothing);
-    });
+      ).toEqual(Nothing));
   });
 });
